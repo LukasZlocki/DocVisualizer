2c6a787408546931dc3bca2b47dc4881976ee638
lzlocki <lzlocki@poczta.fm>
2022-09-12 21:54:07 +0200

    Create document services

---
+++ DocVisualizer.Services/Service/DocumentService.cs
@@ -0,0 +1,22 @@
+﻿using DocVisualizer.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocVisualizer.Services.Service
+{
+    public class DocumentService : IDocumentService
+    {
+        public List<Document> GetAllDocumentsByProduct(string product)
+        {
+            List<Document> documents = new List<Document>();
+
+            // ToDo : code retriving service from database (XML file)
+            // ...
+
+            return documents;
+        }
+    }
+}
---
+++ DocVisualizer.Services/Service/IDocumentService.cs
@@ -0,0 +1,9 @@
+﻿using DocVisualizer.Models.Models;
+
+namespace DocVisualizer.Services.Service
+{
+    internal interface IDocumentService
+    {
+        public List<Document> GetAllDocumentsByProduct(string product);
+    }
+}
