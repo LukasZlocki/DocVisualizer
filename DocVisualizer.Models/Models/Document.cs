6d07b3d910c32ab016d62082630ea803b0dd541c
lzlocki <lzlocki@poczta.fm>
2022-09-12 21:04:11 +0200

    Create document model

---
+++ DocVisualizer.Models/Models/Document.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocVisualizer.Models.Models
+{
+    public class Document
+    {
+        public string DocumentName { get; set; }
+    }
+}
